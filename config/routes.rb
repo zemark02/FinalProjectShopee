a0e0db4ec0935804ff3d619c726e04f5196a5b27
mark <makhawanz@hotmail.com>
2022-01-17 03:06:43 +0700

    commit second

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :stores
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "main#login" 
+  root to: "main#login"
+  get 'login' ,to:"main#login"
   post 'login' ,to: 'main#check_valid_login'
   get "feed/:id" ,to: "users#feed"
   get 'favou/:id' , to: "users#showFollow"
