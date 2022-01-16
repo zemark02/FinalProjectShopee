f6b166af44744e05b1305094a5eebc2de00645b2
mark <makhawanz@hotmail.com>
2022-01-17 01:43:07 +0700

    deploy-heroku2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :stores
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'login' ,to:"main#login"
+  root to: "main#login" 
   post 'login' ,to: 'main#check_valid_login'
   get "feed/:id" ,to: "users#feed"
   get 'favou/:id' , to: "users#showFollow"
